0f9aea2d1ce909341e16e1699b9f48d7232a99ee
SubhamPanigrahi <subhamp.official@gmail.com>
2020-10-10 22:28:39 +0530

    Project Initialization

---
+++ app.js
@@ -0,0 +1,22 @@
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.getElementById('nxt_theme').innerHTML = (themeName === 'theme-dark') ?
+            'Dark <i class="fas fa-moon  fa-lg  "></i>' : 
+            'Light <i class="fas fa-sun fa-lg  "></i>';
+    document.documentElement.className = themeName;
+}
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-light');
+    } else {
+        setTheme('theme-dark');
+    }
+}
+
+(function () {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-dark');
+    } else {
+        setTheme('theme-light');
+    }
+})();
